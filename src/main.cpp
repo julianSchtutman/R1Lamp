a806f6cc2f6a514eca228e4d604052b3d139e514
Julian Schtutman <julianscht@gmail.com>
2025-11-24 16:03:05 -0300

    add logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,51 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Pines del encoder
+const int pinCLK = D5;   // A
+const int pinDT  = D6;   // B
+const int pinSW  = D7;   // Switch (botón)
+
+// Estado previo
+int lastCLK = HIGH;
+int encoderValue = 0;
+
+unsigned long lastButtonTime = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+
+    pinMode(pinCLK, INPUT_PULLUP);
+    pinMode(pinDT,  INPUT_PULLUP);
+    pinMode(pinSW,  INPUT_PULLUP);
+
+    lastCLK = digitalRead(pinCLK);
+
+    Serial.println("Encoder listo!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    // --------- Lectura de giro ----------
+    int currentCLK = digitalRead(pinCLK);
+
+    if (currentCLK != lastCLK) {               // hubo cambio en A
+        if (digitalRead(pinDT) != currentCLK)  // determina la dirección
+            encoderValue++;
+        else
+            encoderValue--;
+
+        Serial.print("Encoder: ");
+        Serial.println(encoderValue);
+    }
+
+    lastCLK = currentCLK;
+
+
+    // --------- Lectura del switch ----------
+    if (digitalRead(pinSW) == LOW) {
+        if (millis() - lastButtonTime > 250) { // debounce
+            Serial.println("Click!");
+        }
+        lastButtonTime = millis();
+    }
 }
