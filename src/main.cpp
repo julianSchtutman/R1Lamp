72911bd9653c40bc5c6a29c109f1ca55d59e4cff
Julian Schtutman <julianscht@gmail.com>
2026-03-03 22:51:33 -0300

    apagado con click

--- src/main.cpp
+++ src/main.cpp
@@ -11,22 +11,35 @@ const uint8_t PIN_BUTTON   = 25;
 const uint8_t PIN_WARM_PWM = 18;  
 const uint8_t PIN_COOL_PWM = 19;  
 
-Encoder enc(PIN_ENC_A, PIN_ENC_B, PIN_BUTTON);  
+static const uint8_t ENCODER_MIN_POS   = 1;
+static const uint8_t ENCODER_MAX_POS   = 10;
+static const uint8_t ENCODER_START_POS = 3;
+
+static bool lampOn = true;
+static PwmLevels lastOnTarget = {100, 0};
+
+Encoder enc(PIN_ENC_A,
+            PIN_ENC_B,
+            PIN_BUTTON,
+            ENCODER_START_POS,
+            ENCODER_MIN_POS,
+            ENCODER_MAX_POS); 
+
 GammaCurve gC(2.6f);
-//Composer   composer(
-//    0.5f,  // coolStart: desde x% de brillo empieza a entrar el frío
-//    0.75f,  // coolEnd:   a x% ya llegó a su mezcla máxima
-//    0.20f, // coolMaxShare: a lo sumo x% del budget se va al frío
-//    2.0f   // coolGamma: entra suave al principio, más decidido al final
-//);
+Composer   composer(
+    0.5f,  // coolStart: desde x% de brillo empieza a entrar el frío
+    0.75f,  // coolEnd:   a x% ya llegó a su mezcla máxima
+    0.20f, // coolMaxShare: a lo sumo x% del budget se va al frío
+    2.0f   // coolGamma: entra suave al principio, más decidido al final
+);
 
 //Solo canal warm
-Composer   composer(
-    1.0f,  
-    1.0f,  
-    0.0f, 
-    2.0f   
-);
+//Composer   composer(
+//    1.0f,  
+//    1.0f,  
+//    0.0f, 
+//    2.0f   
+//);
 
 PwmMapper pwmMapper(
     100,   // pwmWarmMin
@@ -51,45 +64,45 @@ void setup() {
 void loop() {
     enc.update();
 
-    int pos = enc.getAbsolutePosition();  // 0..19
-    static int lastPos = -1;
+    // Toggle ON/OFF con long click
+    if (enc.wasClicked()) {
+        lampOn = !lampOn;
 
-    if (pos != lastPos) {
-        lastPos = pos;
-        Serial.print("Posicion: ");
-        Serial.println(pos);
-        float normPos = enc.getNormalizedPosition();
-        Serial.print("Posicion normalizada: ");
-        Serial.println(normPos);
-        
-        float brightness = gC.apply(normPos);
-        Serial.print("Posicion gamma: ");
-        Serial.println(brightness);
-        
-        LightMix mix     = composer.compose(brightness);
-        Serial.print("Warm: ");
-        Serial.print(mix.warm);
-        Serial.print(" - Cool: ");
-        Serial.println(mix.cool);
-
-        PwmLevels targetPwm = pwmMapper.toPwm(mix);
-        Serial.print("Warm: pwm ");
-        Serial.print(targetPwm.warm);
-        Serial.print(" - Cool pwm: ");
-        Serial.println(targetPwm.cool);
-            
-        transition.setTarget(targetPwm);        
+        if (!lampOn) {
+            Serial.println("Apagar.........");
+            transition.setTarget({0, 0});
+            //enc.freeze();
+        } else {
+            Serial.println("Encender........");
+            transition.setTarget(lastOnTarget);
+            enc.unFreeze();
+        }
     }
 
-    // Avanzamos la transición según el tiempo
+    int pos = enc.getAbsolutePosition();
+    static int lastPos = -1;
+
+    // Solo aplico cambios del encoder si está encendida
+    if (lampOn && pos != lastPos) {
+        lastPos = pos;
+
+        float normPos = enc.getNormalizedPosition();
+        float brightness = gC.apply(normPos);
+        LightMix mix = composer.compose(brightness);
+        PwmLevels targetPwm = pwmMapper.toPwm(mix);
+
+        // guardo último target “encendido”
+        if (targetPwm.warm != 0 || targetPwm.cool != 0) {
+            lastOnTarget = targetPwm;
+        }
+
+        transition.setTarget(targetPwm);
+    }
+
     transition.update();
 
     if (enc.wasClicked()) {
         Serial.println("CLICK!");
-    }
-
-    if (enc.wasLongClicked()) {
-        Serial.println("LONG CLICKED!");
     }
 
     delay(1);
